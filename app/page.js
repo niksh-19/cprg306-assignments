c57422b573c1b7c25e7adc49492d0f63b3feca6f
niksh-19 <nikita.sharma@edu.sait.ca>
2024-09-26 08:36:15 -0600

    week 2 assignment

--- app/page.js
+++ app/page.js
@@ -1,9 +1,16 @@
+import Link from "next/link";
 
 
 export default function Home() {
+
+  let linkStyles = "text-cyan-600 underline hover:text-cyan-300"
+
   return (
     <main>
-      
+      <h2> CPRG 306: Web Development 2 - Assignments</h2>
+    <ul>
+      <li><Link href="./week-2/" className={linkStyles}>Week 2 Assignment</Link></li>
+    </ul>
     </main>
   );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+
+    return(
+        <main>
+            <h1>Nikita Sharma</h1>
+            <Link href="https://github.com/niksh-19/cprg306-assignments" className="text-cyan-600 underline hover:text-cyan-300">https://github.com/niksh-19/cprg306-assignments</Link>
+        </main>
+    );
+}
