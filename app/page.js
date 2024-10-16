e31305a985d9111a40ca4e64e694f283c9176ccb
niksh-19 <nikita.sharma@edu.sait.ca>
2024-10-15 20:02:26 -0600

    week 5

--- app/page.js
+++ app/page.js
@@ -12,6 +12,7 @@ export default function Home() {
       <li><Link href="./week-2/" className={linkStyles}>Week 2 Assignment</Link></li>
       <li><Link href="./week-3/" className={linkStyles}>Week 3 Assignment</Link></li>
       <li><Link href="./week-4/" className={linkStyles}>Week 4 Assignment</Link></li>
+      <li><Link href="./week-5/" className={linkStyles}>Week 5 Assignment</Link></li>
     </ul>
     </main>
   );
